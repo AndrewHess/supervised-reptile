1e65ac755e484cf2353c4c8cdb0a30c619be6d12
Andrew Hess <ahess3815@gmail.com>
2018-05-18 16:01:43 -0700

    changed the train and test folder

--- run_kdd.py
+++ run_kdd.py
@@ -1,8 +1,15 @@
 """
 Train a model on kdd.
-
+    Normal KDD dataset using autoencoded data:
     python -u run_kdd.py --shots 1 --inner-batch 5 --inner-iters 12 --meta-step 1 --meta-batch 3 --meta-iters 200000 --eval-batch 5 --eval-iters 86 --learning-rate 0.00044 --meta-step-final 0 --train-shots 12 --checkpoint ckpt_k15 --pretrained
 
+    Autoencoded KDD dataset with only normal and abnormal classes:
+    python -u run_kdd.py --shots 10 --classes 2 --inner-batch 5 --inner-iters 12 --meta-step 1 --meta-batch 3 --meta-iters 200000 --eval-batch 5 --eval-iters 86 --learning-rate 0.00044 --meta-step-final 0 --train-shots 12 --checkpoint ckpt_kbinary15 --pretrained
+
+    Autoencoded KDD dataset with training on normal and smurf and testing on normal and neptune
+    python -u run_kdd.py --inner-batch 5 --inner-iters 12 --meta-step 1 --meta-batch 3 --meta-iters 200000 --eval-batch 5 --eval-iters 86 --learning-rate 0.00044 --meta-step-final 0 --train-shots 12 --shots 10 --classes 2 --checkpoint ckpt_k2attacks15 --pretrained
+
+    Change DATA_DIR to get data from the correct location
 """
 
 import random
@@ -15,7 +22,7 @@ from supervised_reptile.models import KDDModel
 from supervised_reptile.kdd import read_dataset, split_dataset
 from supervised_reptile.train import train
 
-DATA_DIR = 'data/kdd.nosync'
+DATA_DIR = 'data/kdd_binary.nosync'
 
 def main():
     """
@@ -24,12 +31,16 @@ def main():
     args = argument_parser().parse_args()
     random.seed(args.seed)
 
-    train_set, test_set = split_dataset(read_dataset(DATA_DIR))
+    #train_set, test_set = split_dataset(read_dataset(DATA_DIR))
+
+    train_set, _ = split_dataset(read_dataset(DATA_DIR + '/train'), num_train=args.classes)
+    _, test_set  = split_dataset(read_dataset(DATA_DIR + '/test'),  num_train=0)
+
     train_set = list(train_set)
     test_set = list(test_set)
 
-    print('train_set:', train_set)
-    print('\ntest_set:', test_set)
+    print('train set size:', len(train_set))
+    print('test set size:', len(test_set))
 
     model = KDDModel(args.classes, **model_kwargs(args))
 
@@ -45,11 +56,12 @@ def main():
 
         if not args.test:
             print('Training...')
-            train(sess, model, train_set, test_set, args.checkpoint, resume_itr, **train_kwargs(args))
+            #train(sess, model, train_set, test_set, args.checkpoint, resume_itr, **train_kwargs(args))
+            train(sess, model, train_set, train_set, args.checkpoint, resume_itr, **train_kwargs(args))
 
         print('Evaluating...')
         eval_kwargs = evaluate_kwargs(args)
-        print('Train accuracy: ' + str(evaluate(sess, model, train_set, **eval_kwargs)))
+#        print('Train accuracy: ' + str(evaluate(sess, model, train_set, **eval_kwargs)))
         print('Test accuracy: ' + str(evaluate(sess, model, test_set, **eval_kwargs)))
 
 if __name__ == '__main__':
